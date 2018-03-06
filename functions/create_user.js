0f0d6df6e809b8686e549f2c96846332caa4ca33
Vahan Asryan <v.asryan@apollobytes.com>
2018-03-06 23:12:27 +0400

    finilize createUser function

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,3 +1,23 @@
+const admin = require('firebase-admin');
+
 module.exports = (request, response) => {
-    response.send(request.body);  
+    // Verify the user provided a phone
+    if (!request.body.phone) {
+        return response.status(422)
+            .send({ error: 'Bad Input' });
+    }
+    
+    // Format the phone number
+    const phone = String(request.body.phone)
+        // Remove any none digit symbols
+        .replace(/[^\d]/g, '');
+        
+    // Create a new user account using the phone number
+    admin.auth().createUser({ uid: phone })
+        .then((user) => {
+            return response.send(user);
+        })
+        .catch((err) => {
+            return response.status(422).send({ error: err });
+        });
 };
